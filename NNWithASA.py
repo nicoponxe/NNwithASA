f515d2e38e45524cbf74b0de2d6e793e7e45bac8
Nicolas Ponce <nicohilton@live.com.ar>
2023-05-02 21:00:29 -0300

    Update NNWithASA.py

--- NNWithASA.py
+++ NNWithASA.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def sigmoide(x, deriv=False):
+def sigmoid(x, deriv=False):
     if deriv:
         return x*(1-x)
     else:
@@ -8,7 +9,6 @@ def sigmoide(x, deriv=False):
 
 def f(x, cache, epsilon):
     cache += x**2
-
     adjusted_learning_rate = learning_rate / (np.sqrt(cache) + epsilon)
     return x * adjusted_learning_rate
 
@@ -22,12 +22,16 @@ cache = np.zeros_like(X)
 learning_rate = 2.5
 epsilon = 1e-8
 
-for iter in range(800):
+errors = []
+
+for iter in range(500):
     capa0 = X
-    capa1 = sigmoide(np.dot(capa0, pesos0))
+    capa1 = sigmoid(np.dot(capa0, pesos0))
 
     capa1_error = y - capa1
-    capa1_delta = capa1_error * sigmoide(capa1, True)
+    errors.append(np.mean(np.abs(capa1_error))) #  Calculo y guardo error en cada iteración
+
+    capa1_delta = capa1_error * sigmoid(capa1, True)
 
     f_X = f(capa0, cache, epsilon)
 
@@ -37,3 +41,10 @@ print("Resultado esperado:")
 print(y)
 print("Resultado luego del entrenamiento:")
 print(capa1)
+
+# Plot el error
+plt.plot(errors)
+plt.title('Error')
+plt.xlabel('Iteration')
+plt.ylabel('Mean Absolute Error')
+plt.show()
